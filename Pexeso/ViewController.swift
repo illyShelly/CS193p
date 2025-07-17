5bfc2df885242ddc217caced064cd68da16d0e06
Ilona Sellenberk <ilona.sell@gmail.com>
2025-07-17 15:24:29 +0200

    initial game coded

--- Pexeso/ViewController.swift
+++ Pexeso/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isFaceUp = true // tracking flip state
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func pressButton(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
     }
-
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        //        👈 Keep the font big
+        let attributes: [NSAttributedString.Key: Any] = [
+                 .font: UIFont.systemFont(ofSize: 50)
+             ] // 👈 Keep the font big
+        var attributedEmoji = NSAttributedString(string: "", attributes: attributes)
+        
+        // Showing emoji, flip down
+        if isFaceUp { // button.currentTitle == emoji
+            //          button.setTitle("", for: UIControl.State.normal)
+            //          button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+            attributedEmoji = NSAttributedString(string: "", attributes: attributes)
+            button.setAttributedTitle(attributedEmoji, for: .normal)
+            button.backgroundColor = .systemOrange
+        } else {
+            // Using setAttributedTitle(...) bypasses UIKit's buggy font inheritance or layout pass, and gives total control over the font rendering regardless of what UIKit's internal layout engine does
+            attributedEmoji = NSAttributedString(string: emoji, attributes: attributes)
+            button.setAttributedTitle(attributedEmoji, for: .normal)
+            button.backgroundColor = .white
+            
+            // Force re-layout if needed
+             button.setNeedsLayout()
+             button.layoutIfNeeded()
+            
+//             print("Font after setting: \(button.titleLabel?.font.debugDescription ?? "nil")")
+        }
+        isFaceUp.toggle()
+    }
 
 }
 
