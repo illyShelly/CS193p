f9d5b5013fd2e8e769e58751f6830b8b53061a5d
Ilona Sellenberk <ilona.sell@gmail.com>
2025-07-17 21:25:37 +0200

    create new models - card struct and pexeso class

---
+++ Pexeso/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Pexeso
+//
+//  Created by Ilona Sellenberkova on 17/07/2025.
+//
+
+import Foundation
+
+//Model is UI independent - > so no emoji here
+
+struct Card {
+    var isFacedUP: Bool = false
+    var isMatched: Bool = false
+    var id: Int
+}
---
+++ Pexeso/Pexeso.swift
@@ -0,0 +1,17 @@
+//
+//  Pexeso.swift
+//  Pexeso
+//
+//  Created by Ilona Sellenberkova on 17/07/2025.
+//
+
+import Foundation
+
+class Pexeso {
+    
+    var cards: [Card] = [] // cards = Array<Card>() or [Card]()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
