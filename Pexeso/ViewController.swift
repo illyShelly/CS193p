afcaf706260989266aa01a5e75f6f7711ce70f75
Ilona Sellenberk <ilona.sell@gmail.com>
2025-07-17 17:58:00 +0200

    show cards from collection array, without using MVC

--- Pexeso/ViewController.swift
+++ Pexeso/ViewController.swift
@@ -18,41 +18,40 @@ class ViewController: UIViewController {
     
     @IBOutlet var cardButtons: [UIButton]!
     
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
     @IBAction func pressButton(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
-            print("cardNumber: \(cardNumber)")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
         } else {
             print("chosen card not found in cardButtons")
         }
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
-        let attributes: [NSAttributedString.Key: Any] = [
-                 .font: UIFont.systemFont(ofSize: 50)
-             ] // 👈 Keep the font big
-        var attributedEmoji = NSAttributedString(string: "", attributes: attributes)
+        let attributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 50)]
         
         // Showing emoji, flip down
-        if button.currentTitle == emoji {
-            //          button.setTitle("", for: UIControl.State.normal)
-            //          button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
-            attributedEmoji = NSAttributedString(string: "", attributes: attributes)
-            button.setAttributedTitle(attributedEmoji, for: .normal)
+        if button.attributedTitle(for: .normal)?.string == emoji {
+            let emptyTitle = NSAttributedString(string: "", attributes: attributes)
+            button.setAttributedTitle(emptyTitle, for: .normal)
             button.backgroundColor = .systemOrange
-        } else {
-            // Using setAttributedTitle(...) bypasses UIKit's buggy font inheritance or layout pass, and gives total control over the font rendering regardless of what UIKit's internal layout engine does
-            attributedEmoji = NSAttributedString(string: emoji, attributes: attributes)
+        } else { // Back of the card, flip Up
+            let attributedEmoji = NSAttributedString(string: emoji, attributes: attributes)
             button.setAttributedTitle(attributedEmoji, for: .normal)
             button.backgroundColor = .white
             
             // Force re-layout if needed
              button.setNeedsLayout()
              button.layoutIfNeeded()
-            
-//             print("Font after setting: \(button.titleLabel?.font.debugDescription ?? "nil")")
         }
     }
 
 }
 
+// Using setAttributedTitle(...) bypasses UIKit's buggy font inheritance or layout pass, and gives total control over the font rendering regardless of what UIKit's internal layout engine does
+//          button.setTitle("", for: UIControl.State.normal)
+//          button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+
+//          print("Font after setting: \(button.titleLabel?.font.debugDescription ?? "nil")")
